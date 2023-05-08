ea1802b382ed7c7e6f51d13a97bf399540b3bda6
Pierre Gaillard <pierre.gaillard.home@gmail.com>
2023-05-08 16:47:45 +0200

    add: Event database

---
+++ project/Spote_api/src/Entity/Event.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+#[ApiResource]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
